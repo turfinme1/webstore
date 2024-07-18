a33dba188a3d500566c7bec927769aefb70e9c1c
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-18 23:17:55 +0300

    WIP: create server and handle get and post requests

--- src/index.js
+++ src/index.js
@@ -1,100 +1,74 @@
-const { Client } = require("pg");
-const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "postgres",
-  port: "5432",
+import pg from "pg";
+import config from "./database/dbConfig.js";
+import http from "http";
+import path from "path";
+import fs from "fs";
+const { Pool } = pg;
+const __dirname = import.meta.dirname;
+
+const PORT = 3000;
+
+const server = http.createServer(async (req, res) => {
+  const pool = new Pool(config);
+  const client = await pool.connect();
+
+  switch (req.url) {
+    case "/":
+      const filePath = path.join(__dirname, "views", "index.html");
+      // console.log(filePath);
+      fs.readFile(filePath, "utf-8", (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+      break;
+    case "/js/searchScript.js":
+      const jsFilePath = path.join(__dirname, "js", "searchScript.js");
+      fs.readFile(jsFilePath, "utf-8", (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        res.end(data);
+      });
+      break;
+
+    case "/settlements":
+      const body = await getRequestBody(req);
+
+      try {
+        const parsedData = await JSON.parse(body);
+        console.log("Received JSON data:", parsedData);
+
+        const queryText =
+          'SELECT nm.ekatte,nm.name as "naseleno",km.name as "kmetstvo", ob.name as "obshtina", obl.name as "oblast" FROM public.naseleno_mqsto nm LEFT JOIN public.kmetstvo km ON km.id = nm.kmetstvo_id LEFT JOIN public.obshtina ob ON ob.id = km.obshtina_id LEFT JOIN public.oblast obl ON obl.id = ob.oblast_id WHERE nm.name = $1';
+        const result = await client.query(queryText, [parsedData.searchValue]);
+        console.log(result.rows);
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(result.rows));
+      } catch (e) {
+        console.log(e);
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Invalid JSON" }));
+      }
+      break;
+    default:
+      break;
+  }
 });
 
-const oblasti = require("./data/ek_oblast.json");
-const obshtini = require("./data/ek_obshtina.json");
-const kmetstva = require("./data/ek_kmetstvo.json");
-const naseleniMesta = require("./data/ek_naseleno.json");
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-const seedDatabase = async () => {
-  await client.connect();
-  await seedOblasti(client);
-  await seedObshtini(client);
-  await seedKmetstva(client);
-  await seedNaselenoMqsto(client);
-};
-
-const seedOblasti = async (client) => {
-  const dataset = oblasti.map((ob) => {
-    return [ob.oblast, ob.name_en, ob.name];
+function getRequestBody(req) {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      resolve(body);
+    });
+    req.on("error", (err) => {
+      reject(err);
+    });
   });
-  console.log(dataset);
-
-  const queryString =
-    "INSERT INTO oblast(oblast_code, name_en, name) VALUES($1, $2, $3) RETURNING *";
-  for (const values of dataset) {
-    const res = await client.query(queryString, values);
-    console.log(res.rows[0]);
-  }
-};
-
-const seedObshtini = async (client) => {
-  const dataset = await Promise.all(
-    obshtini.map(async (ob) => {
-      const oblastCode = ob.obshtina.substring(0, 3);
-      const queryString = "SELECT id FROM oblast WHERE oblast_code = $1";
-      const res = await client.query(queryString, [oblastCode]);
-
-      const oblastId = res.rows[0].id;
-      return [ob.obshtina, ob.name_en, ob.name, oblastId];
-    })
-  );
-  console.log(dataset);
-
-  const queryString =
-    "INSERT INTO obshtina(obshtina_code, name_en, name, oblast_id) VALUES($1, $2, $3, $4) RETURNING *";
-  for (const values of dataset) {
-    const res = await client.query(queryString, values);
-    console.log(res.rows[0]);
-  }
-};
-
-const seedKmetstva = async (client) => {
-  const dataset = await Promise.all(
-    kmetstva.map(async (km) => {
-      const obshtinaCode = km.kmetstvo.substring(0, 5);
-      const queryString = "SELECT id FROM obshtina WHERE obshtina_code = $1";
-      const res = await client.query(queryString, [obshtinaCode]);
-      const obshtinaId = res.rows[0].id;
-      return [km.kmetstvo, km.name_en, km.name, obshtinaId];
-    })
-  );
-  console.log(dataset);
-
-  const queryString =
-    "INSERT INTO kmetstvo(kmetstvo_code, name_en, name, obshtina_id) VALUES($1, $2, $3, $4) RETURNING *";
-  for (const values of dataset) {
-    const res = await client.query(queryString, values);
-    console.log(res.rows[0]);
-  }
-};
-
-const seedNaselenoMqsto = async (client) => {
-  let dataset = await Promise.all(
-    naseleniMesta.map(async (nm) => {
-      const kmetstvoCode = nm.kmetstvo;
-      const queryString = "SELECT id FROM kmetstvo WHERE kmetstvo_code = $1";
-      const res = await client.query(queryString, [kmetstvoCode]);
-      if (res.rows[0]) {
-        const kmetstvoId = res.rows[0].id;
-        return [nm.ekatte, nm.name_en, nm.name, kmetstvoId];
-      }
-      return [nm.ekatte, nm.name_en, nm.name, null];
-    })
-  );
-  console.log(dataset);
-  const queryString =
-    "INSERT INTO naseleno_mqsto(ekatte, name_en, name, kmetstvo_id) VALUES($1, $2, $3, $4) RETURNING *";
-  for (const values of dataset) {
-    const res = await client.query(queryString, values);
-    console.log(res.rows[0]);
-  }
-};
-
-seedDatabase();
+}
