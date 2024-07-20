0b5e993b55bd1fccf9ad2c88dbf448bc8e8c6de1
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-20 14:30:17 +0300

    WIP: add endpoints for serving css file and statistics json

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import fileImport from "./database/fileImport.js";
 const PORT = 3000;
 const pool = new Pool(config);
 
-await fileImport(await pool.connect());
+// await fileImport(await pool.connect());
 
 const server = http.createServer(async (req, res) => {
   const client = await pool.connect();
@@ -39,8 +39,7 @@ const server = http.createServer(async (req, res) => {
         const parsedData = await JSON.parse(body);
         console.log("Received JSON data:", parsedData);
 
-        const queryText =
-          'SELECT nm.ekatte,nm.name as "naseleno",km.name as "kmetstvo", ob.name as "obshtina", obl.name as "oblast" FROM public.naseleno_mqsto nm LEFT JOIN public.kmetstvo km ON km.id = nm.kmetstvo_id LEFT JOIN public.obshtina ob ON ob.id = km.obshtina_id LEFT JOIN public.oblast obl ON obl.id = ob.oblast_id WHERE nm.name = $1';
+        const queryText = `SELECT nm.ekatte,nm.name as "naseleno",COALESCE (km.name, 'not found') as "kmetstvo",COALESCE (ob.name, 'not found') as "obshtina",COALESCE(obl.name, 'not found') as "oblast" FROM public.naseleno_mqsto nm LEFT JOIN public.kmetstvo km ON km.id = nm.kmetstvo_id LEFT JOIN public.obshtina ob ON ob.id = km.obshtina_id LEFT JOIN public.oblast obl ON obl.id = ob.oblast_id WHERE LOWER(nm.name) = LOWER($1)`;
         const result = await client.query(queryText, [parsedData.searchValue]);
         console.log(result.rows);
 
@@ -53,6 +52,23 @@ const server = http.createServer(async (req, res) => {
       } finally {
         client.release();
       }
+      break;
+
+    case "/css/index.css":
+      const cssFilePath = path.join(__dirname, "css", "index.css");
+      fs.readFile(cssFilePath, "utf-8", (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(data);
+      });
+      break;
+
+    case "/statistics":
+      const queryText = `SELECT (SELECT COUNT(*) FROM naseleno_mqsto) AS countSettlements,(SELECT COUNT(*) FROM kmetstvo) AS countTownHalls,(SELECT COUNT(*) FROM obshtina) AS countMunicipalities,(SELECT COUNT(*) FROM oblast) AS countRegions LIMIT 1;`;
+      const result = await client.query(queryText);
+      console.log(result.rows);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(result.rows[0]));
       break;
     default:
       break;
