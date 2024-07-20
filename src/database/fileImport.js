b5f3fea6ae9d1805ade750b2dc55a931b42df23c
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-20 14:35:44 +0300

    WIP: add function to import with COPY instead of INSERT

--- src/database/fileImport.js
+++ src/database/fileImport.js
@@ -1,15 +1,11 @@
-import config from "./dbConfig.js";
-import pg from "pg";
-const { Pool } = pg;
-
 import oblasti from "../data/ek_oblast.json" with {type:"json"};
 import obshtini from "../data/ek_obshtina.json" with {type:"json"};
 import kmetstva from "../data/ek_kmetstvo.json" with {type:"json"};
 import naseleniMesta from "../data/ek_naseleno.json" with {type:"json"};
+import { from } from "pg-copy-streams";
+import { Readable } from "stream";
 
-const pool = new Pool(config);
-
-const seedDatabase = async (client) => {
+const fileImport = async (client) => {
   await seedOblasti(client);
   await seedObshtini(client);
   await seedKmetstva(client);
@@ -63,5 +59,48 @@ const seedNaselenoMqsto = async (client) => {
   });
 };
 
-await seedDatabase(await pool.connect());
-export default seedDatabase;
+const seedNaselenoMqstoWithCopy = async (client) => {
+  const map = await precomputeKmetstvoIds(client, naseleniMesta);
+  const copyFromQuery =
+      "COPY naseleno_mqsto (ekatte, name_en, name, kmetstvo_id) FROM STDIN WITH CSV NULL 'null'";
+  try {
+    const dataStream = new Readable({
+      read() {
+        naseleniMesta.forEach((nm) => {
+            const csvLine = `${nm.ekatte},${nm.name_en},${nm.name},${map.get(nm.kmetstvo)}\n`;
+            this.push(csvLine);
+        });
+        this.push(null);
+      },
+    });
+
+    const stream = client.query(from(copyFromQuery));
+    dataStream.pipe(stream);
+
+    stream.on("finish", () => {
+      console.log("Data inserted successfully");
+    });
+
+    stream.on("error", (err) => {
+      console.error("Error during COPY operation:", err);
+    });
+  } catch (err) {
+    console.error("Error during database operation:", err);
+  }
+}
+
+const precomputeKmetstvoIds = async (client, naseleniMesta) =>{
+  let map = new Map();
+  const kmetstvoIdSubquery = `(SELECT COALESCE((SELECT id FROM kmetstvo WHERE kmetstvo_code = $1 LIMIT 1), NULL))`;
+
+  for (let nm of naseleniMesta) {
+    if (!map.get(nm.kmetstvo)) {
+      const res = await client.query(kmetstvoIdSubquery, [nm.kmetstvo]);
+      map.set(nm.kmetstvo, res.rows[0].coalesce);
+    }
+  }
+  
+  return map;
+};
+
+export default fileImport;
