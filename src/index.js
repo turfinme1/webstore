66e4de85dc8f2ddb36f220ccb3b7fd6d69f78255
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-21 20:37:42 +0300

    wip: refactor switch statement into route object containing all endpoints

--- src/index.js
+++ src/index.js
@@ -1,95 +1,52 @@
-import pg from "pg";
 import http from "http";
 import path from "path";
 import fs from "fs";
+import pg from "pg";
 const { Pool } = pg;
-const __dirname = import.meta.dirname;
+
 import config from "./database/dbConfig.js";
-import fileImport from "./database/fileImport.js";
+import routes from "./routes/routes.js";
+import {
+  serveFile,
+  getContentType,
+  getFilePath,
+  createResponse,
+} from "./util/requestUtilities.js";
 
 const PORT = 3000;
 const pool = new Pool(config);
 
 // await fileImport(await pool.connect());
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer(async (request, response) => {
   const client = await pool.connect();
+  const { method, url } = request;
+  const extension = path.extname(url);
+  const contentType = getContentType(extension);
+  const filePath = getFilePath(contentType, url, extension);
 
-  switch (req.url) {
-    case "/":
-      const filePath = path.join(__dirname, "views", "index.html");
-      // console.log(filePath);
-      fs.readFile(filePath, "utf-8", (err, data) => {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-      break;
-    case "/js/searchScript.js":
-      const jsFilePath = path.join(__dirname, "js", "searchScript.js");
-      fs.readFile(jsFilePath, "utf-8", (err, data) => {
-        res.writeHead(200, { "Content-Type": "text/javascript" });
-        res.end(data);
-      });
-      break;
+  console.log("extension", extension);
+  console.log("url", url);
+  console.log("method", method);
 
-    case "/settlements":
-      const body = await getRequestBody(req);
+  const userRoutes = routes({ client });
 
-      try {
-        const parsedData = await JSON.parse(body);
-        console.log("Received JSON data:", parsedData);
-
-        const queryText = `SELECT nm.ekatte,nm.name as "naseleno",COALESCE (km.name, 'not found') as "kmetstvo",COALESCE (ob.name, 'not found') as "obshtina",COALESCE(obl.name, 'not found') as "oblast" FROM public.naseleno_mqsto nm LEFT JOIN public.kmetstvo km ON km.id = nm.kmetstvo_id LEFT JOIN public.obshtina ob ON ob.id = km.obshtina_id LEFT JOIN public.oblast obl ON obl.id = ob.oblast_id WHERE LOWER(nm.name) = LOWER($1)`;
-        const result = await client.query(queryText, [parsedData.searchValue]);
-        console.log(result.rows);
-
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(result.rows));
-      } catch (e) {
-        console.log(e);
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Invalid JSON" }));
-      } finally {
-        client.release();
+  const allRoutes = {
+    ...userRoutes,
+    default: async (request, response) => {
+      if (fs.existsSync(filePath)) {
+        await serveFile(filePath, contentType, response);
+      } else {
+        createResponse(response, 404, "text/html", "<h1>404 Not Found</h1>");
       }
-      break;
+    },
+  };
 
-    case "/css/index.css":
-      const cssFilePath = path.join(__dirname, "css", "index.css");
-      fs.readFile(cssFilePath, "utf-8", (err, data) => {
-        res.writeHead(200, { "Content-Type": "text/css" });
-        res.end(data);
-      });
-      break;
-
-    case "/statistics":
-      const queryText = `SELECT (SELECT COUNT(*) FROM naseleno_mqsto) AS countSettlements,(SELECT COUNT(*) FROM kmetstvo) AS countTownHalls,(SELECT COUNT(*) FROM obshtina) AS countMunicipalities,(SELECT COUNT(*) FROM oblast) AS countRegions LIMIT 1;`;
-      const result = await client.query(queryText);
-      console.log(result.rows);
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(result.rows[0]));
-      break;
-    default:
-      break;
-  }
+  const routeEndpoint = `${url}:${method}`;
+  const routeHandler = allRoutes[routeEndpoint] || allRoutes.default;
+  return await routeHandler(request, response);
 });
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-function getRequestBody(req) {
-  return new Promise((resolve, reject) => {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    req.on("end", () => {
-      resolve(body);
-    });
-    req.on("error", (err) => {
-      reject(err);
-    });
-  });
-}
