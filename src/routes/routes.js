6c2e448131cdb33a954571d8609b57b191bd218f
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-22 22:40:39 +0300

    wip: add all route properties into main route object

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,71 +1,11 @@
 import { getRequestBody, createResponse } from "../util/requestUtilities.js";
+import regionRoutes from "./regionRoutes.js";
+import Repository from "../repository/repository.js";
+import municipalityRoutes from "./municipalityRoutes.js";
+import townHallRoutes from "./townHallRoutes.js";
+import settlementRoutes from "./settlementRoutes.js";
 
 const routes = ({ client }) => ({
-  "/regions:POST": async (request, response) => {
-    const queryText =
-      "INSERT INTO oblast(oblast_code, name_en, name) VALUES($1, $2, $3) RETURNING *";
-    const body = await getRequestBody(request);
-    const parsedData = await JSON.parse(body);
-    const params = [parsedData.oblastCode, parsedData.nameEn, parsedData.name];
-    console.log("Received JSON data:", parsedData);
-
-    try {
-      const result = await client.query(queryText, params);
-      console.log(result.rows);
-
-      return createResponse(response, 201, "application/json", result.rows);
-    } catch (e) {
-      console.log(e);
-      return createResponse(response, 400, "application/json", {
-        error: "Region could not be created",
-      });
-    } finally {
-      client.release();
-    }
-  },
-
-  "/settlements:GET": async (request, response) => {
-    const queryText = `SELECT nm.ekatte,nm.name as "naseleno",COALESCE (km.name, 'not found') as "kmetstvo",COALESCE (ob.name, 'not found') as "obshtina",COALESCE(obl.name, 'not found') as "oblast" FROM public.naseleno_mqsto nm LEFT JOIN public.kmetstvo km ON km.id = nm.kmetstvo_id LEFT JOIN public.obshtina ob ON ob.id = km.obshtina_id LEFT JOIN public.oblast obl ON obl.id = ob.oblast_id WHERE LOWER(nm.name) = LOWER($1)`;
-    const body = await getRequestBody(request);
-    const requestData = request.params;
-    try {
-      console.log("Received JSON data:", requestData);
-
-      const result = await client.query(queryText, [requestData.name]);
-      console.log(result.rows);
-
-      return createResponse(response, 200, "application/json", result.rows);
-    } catch (e) {
-      console.log(e);
-      return createResponse(response, 500, "application/json", {
-        error: "Internal Server Error",
-      });
-    } finally {
-      client.release();
-    }
-  },
-
-  "/regions:DELETE": async (request, response) => {
-    const queryText = "DELETE FROM oblast WHERE id = $1 RETURNING *";
-    const requestParams = request.params;
-    console.log("requestParams", requestParams);
-    const params = [requestParams.id];
-
-    try {
-      const result = await client.query(queryText, params);
-      console.log(result.rows);
-
-      return createResponse(response, 200, "application/json", result.rows);
-    } catch (e) {
-      console.log(e);
-      return createResponse(response, 400, "application/json", {
-        error: "Region could not be deleted",
-      });
-    } finally {
-      client.release();
-    }
-  },
-
   "/statistics:GET": async (request, response) => {
     const queryText = `SELECT (SELECT COUNT(*) FROM naseleno_mqsto) AS countSettlements,(SELECT COUNT(*) FROM kmetstvo) AS countTownHalls,(SELECT COUNT(*) FROM obshtina) AS countMunicipalities,(SELECT COUNT(*) FROM oblast) AS countRegions LIMIT 1;`;
 
@@ -83,59 +23,10 @@ const routes = ({ client }) => ({
     }
   },
 
-  "/regions:GET": async (request, response) => {
-    const queryText = "SELECT * FROM public.oblast WHERE id = $1 LIMIT 1";
-    const requestParams = request.params;
-
-    try {
-      console.log("Received JSON data:", requestParams);
-
-      if (requestParams.id) {
-        const result = await client.query(queryText, [requestParams.id]);
-        console.log(result.rows);
-        if (!result.rows[0]) {
-          return createResponse(response, 404, "application/json", result.rows);
-        }
-        return createResponse(response, 200, "application/json", result.rows);
-      }
-    } catch (e) {
-      console.log(e);
-      return createResponse(response, 500, "application/json", {
-        error: "Internal Server Error",
-      });
-    } finally {
-      client.release();
-    }
-  },
-
-  "/regions:PUT": async (request, response) => {
-    const queryText = `UPDATE oblast SET oblast_code = $1, name = $2, name_en = $3 WHERE id = $4 RETURNING *`;
-    const requestParams = request.params;
-    const body = await getRequestBody(request);
-
-    try {
-      const { oblastCode, name, nameEn } = JSON.parse(body);
-      const params = [oblastCode, name, nameEn, requestParams.id];
-
-      const result = await client.query(queryText, params);
-      console.log(result.rows);
-
-      if (result.rows.length === 0) {
-        return createResponse(response, 404, "application/json", {
-          error: "Region not found",
-        });
-      }
-
-      return createResponse(response, 200, "application/json", result.rows);
-    } catch (e) {
-      console.log(e);
-      return createResponse(response, 400, "application/json", {
-        error: "Region could not be updated",
-      });
-    } finally {
-      client.release();
-    }
-  },
+  ...regionRoutes(client, new Repository(client, "oblast")),
+  ...municipalityRoutes(client, new Repository(client, "obshtina")),
+  ...townHallRoutes(client, new Repository(client, "kmetstvo")),
+  ...settlementRoutes(client, new Repository(client, "naseleno_mqsto")),
 });
 
 export default routes;
