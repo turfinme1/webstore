7de5fb1ad0b652cc8e631606bb3a5d6a3904a2b0
Borislav <borislav.m.angelov@gmail.com>
2024-07-22 14:11:38 +0300

    wip: change POST request to insertrecord and return 201 (created) response

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,36 @@ import { getRequestBody, createResponse } from "../util/requestUtilities.js";
 
 const routes = ({ client }) => ({
   "/settlements:POST": async (request, response) => {
-    const queryText = `SELECT nm.ekatte,nm.name as "naseleno",COALESCE (km.name, 'not found') as "kmetstvo",COALESCE (ob.name, 'not found') as "obshtina",COALESCE(obl.name, 'not found') as "oblast" FROM public.naseleno_mqsto nm LEFT JOIN public.kmetstvo km ON km.id = nm.kmetstvo_id LEFT JOIN public.obshtina ob ON ob.id = km.obshtina_id LEFT JOIN public.oblast obl ON obl.id = ob.oblast_id WHERE LOWER(nm.name) = LOWER($1)`;
+    const queryText =
+      "INSERT INTO oblast(oblast_code, name_en, name) VALUES($1, $2, $3) RETURNING *";
     const body = await getRequestBody(request);
+    const parsedData = await JSON.parse(body);
+    const params = [parsedData.oblastCode, parsedData.nameEn, parsedData.name];
+    console.log("Received JSON data:", parsedData);
 
     try {
-      const parsedData = await JSON.parse(body);
-      console.log("Received JSON data:", parsedData);
+      const result = await client.query(queryText, params);
+      console.log(result.rows);
 
-      const result = await client.query(queryText, [parsedData.searchValue]);
+      return createResponse(response, 201, "application/json", result.rows);
+    } catch (e) {
+      console.log(e);
+      return createResponse(response, 400, "application/json", {
+        error: "Region could not be created",
+      });
+    } finally {
+      client.release();
+    }
+  },
+
+  "/settlements:GET": async (request, response) => {
+    const queryText = `SELECT nm.ekatte,nm.name as "naseleno",COALESCE (km.name, 'not found') as "kmetstvo",COALESCE (ob.name, 'not found') as "obshtina",COALESCE(obl.name, 'not found') as "oblast" FROM public.naseleno_mqsto nm LEFT JOIN public.kmetstvo km ON km.id = nm.kmetstvo_id LEFT JOIN public.obshtina ob ON ob.id = km.obshtina_id LEFT JOIN public.oblast obl ON obl.id = ob.oblast_id WHERE LOWER(nm.name) = LOWER($1)`;
+    const body = await getRequestBody(request);
+    const requestData = request.params;
+    try {
+      console.log("Received JSON data:", requestData);
+
+      const result = await client.query(queryText, [requestData.name]);
       console.log(result.rows);
 
       return createResponse(response, 200, "application/json", result.rows);
