fc5877ace0ed82a61a1977b9ee9e2cfda00adb8c
Borislav <borislav.m.angelov@gmail.com>
2024-07-22 12:11:00 +0300

    wip: handle query params properly

--- src/index.js
+++ src/index.js
@@ -23,26 +23,33 @@ const server = http.createServer(async (request, response) => {
   const { method, url } = request;
   const extension = path.extname(url);
   const contentType = getContentType(extension);
-  const filePath = getFilePath(contentType, url, extension);
+  const parsedUrl = new URL(request.url, `http://${request.headers.host}`);
+  const queryObject = Object.fromEntries(parsedUrl.searchParams.entries());
+  request.params = queryObject;
+  const pathName = parsedUrl.pathname;
+  const filePath = getFilePath(contentType, pathName, extension);
 
+  console.log("queryObject", queryObject);
   console.log("extension", extension);
-  console.log("url", url);
   console.log("method", method);
-
+  console.log("request params", request.params);
+  console.log("pathname", parsedUrl.pathname);
   const userRoutes = routes({ client });
 
   const allRoutes = {
     ...userRoutes,
     default: async (request, response) => {
       if (fs.existsSync(filePath)) {
+        client.release();
         await serveFile(filePath, contentType, response);
       } else {
+        client.release();
         createResponse(response, 404, "text/html", "<h1>404 Not Found</h1>");
       }
     },
   };
 
-  const routeEndpoint = `${url}:${method}`;
+  const routeEndpoint = `${pathName}:${method}`;
   const routeHandler = allRoutes[routeEndpoint] || allRoutes.default;
   return await routeHandler(request, response);
 });
