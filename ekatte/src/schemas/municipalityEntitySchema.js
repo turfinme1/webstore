796a2081593bb7be148cae926710ec6a97ade864
Borislav <borislav.m.angelov@gmail.com>
2024-08-19 18:57:47 +0300

    Add additional properties for rendering forms and table

--- ekatte/src/schemas/municipalityEntitySchema.js
+++ ekatte/src/schemas/municipalityEntitySchema.js
@@ -7,36 +7,43 @@ export const municipalitySchema = {
       type: "string",
       minLength: 5,
       pattern: "^[a-zA-Z0-9]+$",
+      label: "Municipality Code",
+      placeholder: "Enter Municipality Code",
       errorMessage: {
         minLength: "Municipality code must be at least 5 characters long.",
-        pattern:
-          "Municipality code must contain only Latin letters and digits.",
+        pattern: "Municipality code must contain only Latin letters and digits.",
       },
     },
     name_en: {
       type: "string",
       minLength: 3,
       pattern: "^[a-zA-Z ]+$",
+      label: "Name (EN)",
+      placeholder: "Enter Name (EN)",
+      searchable: true,
       errorMessage: {
         minLength: "Name (EN) must be at least 3 characters long.",
         pattern: "Name (EN) must contain only Latin letters and spaces.",
       },
-      searchable: true,
     },
     name: {
       type: "string",
       minLength: 3,
       pattern: "^[а-яА-Я ]+$",
+      label: "Name",
+      placeholder: "Enter Name",
+      searchable: true,
       errorMessage: {
         minLength: "Name must be at least 3 characters long.",
         pattern: "Name must contain only Cyrillic letters and spaces.",
       },
-      searchable: true,
     },
     region_id: {
       type: "string",
       minLength: 1,
       pattern: "^[0-9]+$",
+      label: "Region ID",
+      placeholder: "Enter Region ID",
       errorMessage: {
         minLength: "Region ID must not be empty.",
         pattern: "Region ID must contain only digits.",
