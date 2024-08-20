394133c3468517d0047418c5a9a871a1ebe5cd48
Borislav <borislav.m.angelov@gmail.com>
2024-08-20 18:57:13 +0300

    Add views for pagination

--- ekatte/src/database/dbScript.sql
+++ ekatte/src/database/dbScript.sql
@@ -45,3 +45,83 @@ ON town_hall (municipality_id);
 
 CREATE INDEX index_region_id_on_municipality
 ON municipality (region_id);
+
+CREATE OR REPLACE VIEW municipality_view AS
+SELECT 
+	municipality.*,
+    region.name_en AS region_name_en,
+    region.name AS region_name
+FROM 
+    municipality
+JOIN 
+    region ON region.id = municipality.region_id;
+	
+CREATE OR REPLACE VIEW town_hall_view AS
+SELECT 
+    town_hall.id,
+    town_hall.town_hall_code,
+    town_hall.name_en,
+    town_hall.name,
+    municipality.municipality_code,
+    municipality.name_en AS municipality_name_en,
+    municipality.name AS municipality_name,
+    region.region_code AS region_code,
+    region.name_en AS region_name_en,
+    region.name AS region_name
+FROM 
+    town_hall
+JOIN 
+    municipality ON municipality.id = town_hall.municipality_id
+JOIN 
+    region ON region.id = municipality.region_id;
+
+CREATE OR REPLACE VIEW settlement_view AS
+SELECT 
+    settlement.id,
+    settlement.ekatte,
+    settlement.name_en,
+    settlement.name,
+    town_hall.town_hall_code,
+    town_hall.name_en AS town_hall_name_en,
+    town_hall.name AS town_hall_name,
+    municipality.municipality_code,
+    municipality.name_en AS municipality_name_en,
+    municipality.name AS municipality_name,
+    region.region_code AS region_code,
+    region.name_en AS region_name_en,
+    region.name AS region_name
+FROM 
+    settlement
+LEFT JOIN 
+    town_hall ON town_hall.id = settlement.town_hall_id
+LEFT JOIN 
+    municipality ON municipality.id = town_hall.municipality_id
+LEFT JOIN 
+    region ON region.id = municipality.region_id;
+
+CREATE OR REPLACE VIEW full_hierarchy_view AS
+SELECT 
+    settlement.id AS settlement_id,
+    settlement.ekatte,
+    settlement.name_en AS settlement_name_en,
+    settlement.name AS settlement_name,
+    town_hall.id AS town_hall_id,
+    town_hall.town_hall_code,
+    town_hall.name_en AS town_hall_name_en,
+    town_hall.name AS town_hall_name,
+    municipality.id AS municipality_id,
+    municipality.municipality_code,
+    municipality.name_en AS municipality_name_en,
+    municipality.name AS municipality_name,
+    region.id AS region_id,
+    region.region_code,
+    region.name_en AS region_name_en,
+    region.name AS region_name
+FROM 
+    settlement
+LEFT JOIN 
+    town_hall ON town_hall.id = settlement.town_hall_id
+LEFT JOIN 
+    municipality ON municipality.id = town_hall.municipality_id
+LEFT JOIN 
+    region ON region.id = municipality.region_id;
