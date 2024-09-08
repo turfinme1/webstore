5e50b06be7f196cbf679ff56b16180ec0d4cf840
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-09-08 23:00:02 +0300

    chore: Remove getFilteredPaginated endpoint from CRUD controller

--- src/controllers/__tests__/crudController.test.js
+++ src/controllers/__tests__/crudController.test.js
@@ -1,333 +1,116 @@
-const CrudController = require("../crudController");
+const CrudController = require('../crudController');
 
-describe("CrudController", () => {
-  let mockEntitySchemaCollection;
+describe('CrudController', () => {
   let crudController;
-  let mockNext = jest.fn();
+  let crudService;
+  let mockRes;
+  let mockNext;
 
   beforeEach(() => {
-    mockEntitySchemaCollection = {
-      entity1: {
-        name: "entity1",
-        views: "entity1_views",
-        properties: {
-          id: "number",
-          name: "string",
-        },
-      },
-      entity2: {
-        name: "entity2",
-        views: "entity2_views",
-        properties: {
-          id: "number",
-          age: "number",
-        },
-      },
+    crudService = {
+      create: jest.fn(),
+      getById: jest.fn(),
+      getAll: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    };
+    crudController = new CrudController(crudService);
+
+    mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
     };
 
-    crudController = new CrudController(mockEntitySchemaCollection);
+    mockNext = jest.fn();
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  describe('create', () => {
+    it('should call crudService.create and respond with status 201', async () => {
+      const req = { body: { name: 'Test Product' }, params: { entity: 'testEntity' } };
+      const createdProduct = { id: 1, name: 'Test Product' };
 
-  describe("create", () => {
-    it("should create a new entity and return the created entity", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        body: {
-          id: 1,
-          name: "John",
-        },
-        dbConnection: {
-          query: jest.fn().mockResolvedValueOnce({
-            rows: [{ id: 1, name: "John" }],
-          }),
-        },
-      };
-      const mockResponse = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
+      crudService.create.mockResolvedValue([createdProduct]);
 
-      await crudController.create(mockRequest, mockResponse, mockNext);
+      await crudController.create(req, mockRes, mockNext);
 
-      expect(mockRequest.dbConnection.query).toHaveBeenCalledWith(
-        "INSERT INTO entity1(id,name) VALUES($1,$2) RETURNING *",
-        [1, "John"]
-      );
-      expect(mockResponse.status).toHaveBeenCalledWith(201);
-      expect(mockResponse.json).toHaveBeenCalledWith({ id: 1, name: "John" });
-    });
-
-    it("should handle errors and call the error handling middleware", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        body: {
-          id: 1,
-          name: "John",
-        },
-        dbConnection: {
-          query: jest.fn().mockRejectedValue(new Error("Database error")),
-        },
-      };
-      const mockResponse = {};
-      const mockNext = jest.fn();
-
-      await crudController.create(mockRequest, mockResponse, mockNext);
-
-      expect(mockNext).toHaveBeenCalledWith(new Error("Database error"));
+      expect(crudService.create).toHaveBeenCalledWith(req);
+      expect(mockRes.status).toHaveBeenCalledWith(201);
+      expect(mockRes.json).toHaveBeenCalledWith([createdProduct]);
     });
   });
 
-  describe("getById", () => {
-    it("should get an entity by ID and return the entity", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        dbConnection: {
-          query: jest.fn().mockResolvedValueOnce({
-            rows: [{ id: 1, name: "John" }],
-          }),
-        },
-      };
-      const mockResponse = {
-        json: jest.fn(),
-      };
+  describe('getById', () => {
+    it('should call crudService.getById and respond with status 200', async () => {
+      const req = { params: { entity: 'testEntity', id: 1 } };
+      const foundProduct = { id: 1, name: 'Test Product' };
 
-      await crudController.getById(mockRequest, mockResponse, mockNext);
+      crudService.getById.mockResolvedValue(foundProduct);
 
-      expect(mockRequest.dbConnection.query).toHaveBeenCalledWith(
-        "SELECT * FROM entity1_views WHERE id = $1",
-        [1]
-      );
-      expect(mockResponse.json).toHaveBeenCalledWith({ id: 1, name: "John" });
+      await crudController.getById(req, mockRes, mockNext);
+
+      expect(crudService.getById).toHaveBeenCalledWith(req);
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith(foundProduct);
     });
 
-    it("should handle entity not found and return a 404 error", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        dbConnection: {
-          query: jest.fn().mockResolvedValueOnce({
-            rows: [],
-          }),
-        },
-      };
-      const mockResponse = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
+    it('should return a 200 status with null if product is not found', async () => {
+      const req = { params: { entity: 'testEntity', id: 1 } };
 
-      await crudController.getById(mockRequest, mockResponse, mockNext);
+      crudService.getById.mockResolvedValue(null);
 
-      expect(mockRequest.dbConnection.query).toHaveBeenCalledWith(
-        "SELECT * FROM entity1_views WHERE id = $1",
-        [1]
-      );
-      expect(mockResponse.status).toHaveBeenCalledWith(404);
-      expect(mockResponse.json).toHaveBeenCalledWith({
-        error: "Entity not found",
-      });
-    });
+      await crudController.getById(req, mockRes, mockNext);
 
-    it("should handle errors and call the error handling middleware", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        dbConnection: {
-          query: jest.fn().mockRejectedValue(new Error("Database error")),
-        },
-      };
-      const mockResponse = {};
-      const mockNext = jest.fn();
-
-      await crudController.getById(mockRequest, mockResponse, mockNext);
-
-      expect(mockNext).toHaveBeenCalledWith(new Error("Database error"));
+      expect(crudService.getById).toHaveBeenCalledWith(req);
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith(null);
     });
   });
 
-  describe("getAll", () => {
-    it("should get all entities and return an array of entities", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        dbConnection: {
-          query: jest.fn().mockResolvedValueOnce({
-            rows: [
-              { id: 1, name: "John" },
-              { id: 2, name: "Jane" },
-            ],
-          }),
-        },
-      };
-      const mockResponse = {
-        json: jest.fn(),
-      };
+  describe('getAll', () => {
+    it('should call crudService.getAll and respond with status 200', async () => {
+      const req = { params: { entity: 'testEntity' } };
+      const products = [{ id: 1, name: 'Test Product 1' }, { id: 2, name: 'Test Product 2' }];
 
-      await crudController.getAll(mockRequest, mockResponse, mockNext);
+      crudService.getAll.mockResolvedValue(products);
 
-      expect(mockRequest.dbConnection.query).toHaveBeenCalledWith(
-        "SELECT * FROM entity1_views"
-      );
-      expect(mockResponse.json).toHaveBeenCalledWith([
-        { id: 1, name: "John" },
-        { id: 2, name: "Jane" },
-      ]);
-    });
+      await crudController.getAll(req, mockRes, mockNext);
 
-    it("should handle errors and call the error handling middleware", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        dbConnection: {
-          query: jest.fn().mockRejectedValue(new Error("Database error")),
-        },
-      };
-      const mockResponse = {};
-      const mockNext = jest.fn();
-
-      await crudController.getAll(mockRequest, mockResponse, mockNext);
-
-      expect(mockNext).toHaveBeenCalledWith(new Error("Database error"));
+      expect(crudService.getAll).toHaveBeenCalledWith(req);
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith(products);
     });
   });
 
-  describe("update", () => {
-    it("should update an entity and return the updated entity", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        body: {
-          id: 1,
-          name: "John Doe",
-        },
-        dbConnection: {
-          query: jest.fn().mockResolvedValueOnce({
-            rows: [{ id: 1, name: "John Doe" }],
-          }),
-        },
+  describe('update', () => {
+    it('should call crudService.update and respond with status 200', async () => {
+      const req = {
+        params: { entity: 'testEntity', id: 1 },
+        body: { name: 'Updated Product' },
       };
-      const mockResponse = {
-        json: jest.fn(),
-      };
+      const updatedProduct = { id: 1, name: 'Updated Product' };
 
-      await crudController.update(mockRequest, mockResponse, mockNext);
+      crudService.update.mockResolvedValue(updatedProduct);
 
-      expect(mockRequest.dbConnection.query).toHaveBeenCalledWith(
-        "UPDATE entity1 SET id = $1, name = $2 WHERE id = $3 RETURNING *",
-        [1, "John Doe", 1]
-      );
-      expect(mockResponse.json).toHaveBeenCalledWith({
-        id: 1,
-        name: "John Doe",
-      });
-    });
+      await crudController.update(req, mockRes, mockNext);
 
-    it("should handle errors and call the error handling middleware", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        body: {
-          id: 1,
-          name: "John Doe",
-        },
-        dbConnection: {
-          query: jest.fn().mockRejectedValue(new Error("Database error")),
-        },
-      };
-      const mockResponse = {};
-      const mockNext = jest.fn();
-
-      await crudController.update(mockRequest, mockResponse, mockNext);
-
-      expect(mockNext).toHaveBeenCalledWith(new Error("Database error"));
+      expect(crudService.update).toHaveBeenCalledWith(req);
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith(updatedProduct);
     });
   });
 
-  describe("deleteEntity", () => {
-    it("should delete an entity and return a success message", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        dbConnection: {
-          query: jest.fn().mockResolvedValueOnce({
-            rows: [{ id: 1 }],
-          }),
-        },
-      };
-      const mockResponse = {
-        json: jest.fn(),
-      };
+  describe('delete', () => {
+    it('should call crudService.delete and respond with status 200', async () => {
+      const req = { params: { entity: 'testEntity', id: 1 } };
+      const deletedProduct = { id: 1, name: 'Deleted Product' };
 
-      await crudController.deleteEntity(mockRequest, mockResponse, mockNext);
+      crudService.delete.mockResolvedValue(deletedProduct);
 
-      expect(mockRequest.dbConnection.query).toHaveBeenCalledWith(
-        "DELETE FROM entity1 WHERE id = $1 RETURNING *",
-        [1]
-      );
-      expect(mockResponse.json).toHaveBeenCalledWith({
-        message: "Entity deleted",
-      });
-    });
+      await crudController.delete(req, mockRes, mockNext);
 
-    it("should handle entity not found and return a 404 error", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        dbConnection: {
-          query: jest.fn().mockResolvedValueOnce({
-            rows: [],
-          }),
-        },
-      };
-      const mockResponse = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-
-      await crudController.deleteEntity(mockRequest, mockResponse, mockNext);
-
-      expect(mockRequest.dbConnection.query).toHaveBeenCalledWith(
-        "DELETE FROM entity1 WHERE id = $1 RETURNING *",
-        [1]
-      );
-      expect(mockResponse.status).toHaveBeenCalledWith(404);
-      expect(mockResponse.json).toHaveBeenCalledWith({
-        error: "Entity not found",
-      });
-    });
-
-    it("should handle errors and call the error handling middleware", async () => {
-      const mockRequest = {
-        url: "/crud/entity1",
-        params: {
-          id: 1,
-        },
-        dbConnection: {
-          query: jest.fn().mockRejectedValue(new Error("Database error")),
-        },
-      };
-      const mockResponse = {};
-      const mockNext = jest.fn();
-
-      await crudController.deleteEntity(mockRequest, mockResponse, mockNext);
-
-      expect(mockNext).toHaveBeenCalledWith(new Error("Database error"));
+      expect(crudService.delete).toHaveBeenCalledWith(req);
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.json).toHaveBeenCalledWith(deletedProduct);
     });
   });
 });
--- src/controllers/crudController.js
+++ src/controllers/crudController.js
@@ -6,7 +6,6 @@ class CrudController {
     this.getById = this.getById.bind(this);
     this.update = this.update.bind(this);
     this.delete= this.delete.bind(this);
-    this.getFilteredPaginated = this.getFilteredPaginated.bind(this);
   }
 
   async create(req, res, next) {
@@ -32,105 +31,7 @@ class CrudController {
   async delete(req, res, next) {
     const result = await this.crudService.delete(req);
     res.status(200).json(result);
-  }
-
-  async getFilteredPaginated(req, res, next) {
-    const schema = this.entitySchemaCollection[req.params.entity];
-    const viewName = schema.views;
-    let searchParams = req.query.searchParams
-      ? JSON.parse(req.query.searchParams)
-      : {};
-    let orderParams = req.query.orderParams
-      ? JSON.parse(req.query.orderParams)
-      : [];
-    let page = req.query.page || 1;
-    let pageSize = req.query.pageSize || 30;
-    const offset = (page - 1) * pageSize;
-    const connection = req.dbConnection;
-
-    let searchValues = [];
-    let keywordSearchConditions = [];
-    let filterConditions = [];
-
-    if (searchParams.keyword) {
-      const searchable = Object.keys(schema.displayProperties).filter(
-        (property) => schema.displayProperties[property].searchable
-      );
-
-      for (const property of searchable) {
-        searchValues.push(searchParams.keyword);
-        keywordSearchConditions.push(
-          `STRPOS(LOWER(CAST(${property} AS text)), LOWER($${searchValues.length})) > 0`
-        );
-      }
-    }
-
-    if (searchParams.price) {
-      if (searchParams.price.min) {
-        searchValues.push(searchParams.price.min);
-        filterConditions.push(`price >= $${searchValues.length}`);
-      }
-      if (searchParams.price.max) {
-        searchValues.push(searchParams.price.max);
-        filterConditions.push(`price <= $${searchValues.length}`);
-      }
-    }
-
-    // Handle category filtering (e.g., categories: ["Electronics", "Books"])
-    if (searchParams.categories && searchParams.categories.length > 0) {
-      const categoryPlaceholders = searchParams.categories
-        .map((_, index) => `$${searchValues.length + index + 1}`)
-        .join(", ");
-      searchValues.push(...searchParams.categories);
-      filterConditions.push(
-        `ARRAY(SELECT LOWER(unnest(categories))) && ARRAY[${categoryPlaceholders}]::text[]`
-      );
-    }
-
-    // Combine keyword conditions with OR and other filter conditions with AND
-    let combinedConditions = [];
-
-    // If there are keyword search conditions, combine them using OR
-    if (keywordSearchConditions.length > 0) {
-      combinedConditions.push(`(${keywordSearchConditions.join(" OR ")})`);
-    }
-
-    // If there are filter conditions, combine them using AND
-    if (filterConditions.length > 0) {
-      combinedConditions.push(`${filterConditions.join(" AND ")}`);
-    }
-
-    // Build final search condition string, combining keyword and filter conditions with AND
-    const searchConditionStr =
-      combinedConditions.length > 0
-        ? `WHERE ${combinedConditions.join(" AND ")}`
-        : "";
-
-    const orderByClause =
-      orderParams.length > 0
-        ? orderParams
-            .map(([column, direction]) => `${column} ${direction}`)
-            .join(", ")
-        : "id";
-
-    let query = `
-      SELECT * FROM ${viewName} 
-      ${searchConditionStr} 
-      ORDER BY ${orderByClause} 
-      LIMIT $${searchValues.length + 1} OFFSET $${searchValues.length + 2}`;
-
-    let countQuery = `SELECT COUNT(*) FROM ${viewName} ${searchConditionStr}`;
-
-    const { rows } = await connection.query(query, [
-      ...searchValues,
-      pageSize,
-      offset,
-    ]);
-    const totalRowCount = await connection.query(countQuery, searchValues);
-    const count = totalRowCount[0].count;
-
-    res.json(rows);
-  }
+  }  
 }
 
 module.exports = CrudController;
