196047d5dc12e5453d72c19c3b02d7d20608edbf
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-09-08 23:02:35 +0300

    feat: Add endpoint for products

--- src/serverConfigurations/serviceConfiguration.js
+++ src/serverConfigurations/serviceConfiguration.js
@@ -1,14 +1,19 @@
 const CrudService = require("../services/crudService");
 const CrudController = require("../controllers/crudController");
+const ProductService = require("../services/productService");
+const ProductController = require("../controllers/productController");
 const entitySchemaCollection = require("../schemas/entitySchemaCollection");
 
 const service = new CrudService(entitySchemaCollection);
 const controller = new CrudController(service);
+const productService = new ProductService(entitySchemaCollection);
+const productController = new ProductController(productService);
 
 const routeTable = {
   get: {
     "/crud/:entity": controller.getAll,
     "/crud/:entity/:id": controller.getById,
+    "/api/products" : productController.getFilteredPaginated,
   },
   post: {
     "/crud/:entity": controller.create,
