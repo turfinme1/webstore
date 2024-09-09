a5b03d3c8072cc7b97c2338dbd1387402f985841
Borislav <borislav.m.angelov@gmail.com>
2024-09-09 11:57:14 +0300

    feat: Add validation for query parameters

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,12 @@
 class ProductController {
-  constructor(productService) {
+  constructor(productService, validateQueryParams) {
     this.productService = productService;
+    this.validateQueryParams = validateQueryParams;
     this.getFilteredPaginated = this.getFilteredPaginated.bind(this);
   }
   
   async getFilteredPaginated(req, res, next) {
+    this.validateQueryParams(req, req.entitySchemaCollection["products"]);
     const result = await this.productService.getFilteredPaginated(req);
     res.status(200).json(result);
   }
--- src/serverConfigurations/serviceConfiguration.js
+++ src/serverConfigurations/serviceConfiguration.js
@@ -3,11 +3,12 @@ const CrudController = require("../controllers/crudController");
 const ProductService = require("../services/productService");
 const ProductController = require("../controllers/productController");
 const entitySchemaCollection = require("../schemas/entitySchemaCollection");
+const { ASSERT_USER } = require("./assert");
 
 const service = new CrudService(entitySchemaCollection);
 const controller = new CrudController(service);
 const productService = new ProductService(entitySchemaCollection);
-const productController = new ProductController(productService);
+const productController = new ProductController(productService, validateQueryParams);
 
 const routeTable = {
   get: {
@@ -26,4 +27,24 @@ const routeTable = {
   },
 };
 
-module.exports = { routeTable };
+function validateQueryParams(req, schema) {
+  ASSERT_USER(schema, "Invalid query parameters");
+  let searchParams = req.query.searchParams ? JSON.parse(req.query.searchParams) : {};
+  let orderParams = req.query.orderParams ? JSON.parse(req.query.orderParams) : [];
+  
+  const invalidSearchParams = Object.keys(searchParams).filter(
+    (key) => !(schema.seachParams[key])
+  );
+  ASSERT_USER(invalidSearchParams.length === 0, "Invalid query parameters");
+
+  const validDirections = ["ASC", "DESC"];
+  const invalidOrderParams = orderParams.filter(
+    ([key, direction]) => 
+      !(schema.properties[key] || schema.displayProperties[key]) || 
+      !validDirections.includes(direction.toUpperCase())
+  );
+  ASSERT_USER(invalidOrderParams.length === 0, "Invalid order query parameters");
+}
+
+
+module.exports = { routeTable, validateQueryParams };
