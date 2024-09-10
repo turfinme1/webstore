32a9b34b60cdf6d2f1429959ac31525e757acf2e
Borislav <borislav.m.angelov@gmail.com>
2024-09-10 11:45:41 +0300

    refactor: Remove CRUD functionality

--- src/serverConfigurations/serviceConfiguration.js
+++ src/serverConfigurations/serviceConfiguration.js
@@ -1,28 +1,13 @@
-const CrudService = require("../services/crudService");
-const CrudController = require("../controllers/crudController");
 const ProductService = require("../services/productService");
 const ProductController = require("../controllers/productController");
 const { ASSERT_USER } = require("./assert");
 
-const service = new CrudService();
-const controller = new CrudController(service);
 const productService = new ProductService();
 const productController = new ProductController(productService, validateQueryParams);
 
 const routeTable = {
   get: {
-    "/crud/:entity": controller.getAll,
-    "/crud/:entity/:id": controller.getById,
     "/api/products" : productController.getFilteredPaginated,
-  },
-  post: {
-    "/crud/:entity": controller.create,
-  },
-  put: {
-    "/crud/:entity/:id": controller.update,
-  },
-  delete: {
-    "/crud/:entity/:id": controller.delete,
   },
 };
 
