0ef13907224e3031bb8f6d09cd630d4155836742
Borislav <borislav.m.angelov@gmail.com>
2024-09-13 13:53:34 +0300

    feat: Add currencies table and iso country codes

--- src/database/dbScript.sql
+++ src/database/dbScript.sql
@@ -1,9 +1,11 @@
 DROP VIEW IF EXISTS products_view;
+DROP VIEW IF EXISTS country_codes_view;
 
 DROP TABLE IF EXISTS products_categories;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS currencies;
 
 CREATE TABLE products (
     id BIGSERIAL PRIMARY KEY,
@@ -11,6 +13,13 @@ CREATE TABLE products (
     price NUMERIC(12, 2) NOT NULL,
     short_description TEXT NOT NULL,
     long_description TEXT NOT NULL
+);
+
+CREATE TABLE currencies (
+    id BIGSERIAL PRIMARY KEY,
+    currency_code TEXT NOT NULL,
+    exchange_rate_to_base NUMERIC(18, 6) NOT NULL, 
+    symbol TEXT NOT NULL,
 );
 
 CREATE TABLE images (
@@ -44,3 +53,19 @@ LEFT JOIN images i ON p.id = i.product_id
 LEFT JOIN products_categories pc ON p.id = pc.product_id
 LEFT JOIN categories c ON pc.category_id = c.id
 GROUP BY p.id;
+
+CREATE OR REPLACE VIEW country_codes_view AS
+SELECT
+    icc.id,
+    icc.country_name,
+    icc.country_code,
+    icc.phone_code
+FROM iso_country_codes icc
+ORDER BY icc.country_name;
+
+CREATE TABLE iso_country_codes (
+    id BIGSERIAL PRIMARY KEY,
+    country_name TEXT UNIQUE NOT NULL,                
+    country_code TEXT UNIQUE NOT NULL,      
+    phone_code TEXT NOT NULL                   
+);
