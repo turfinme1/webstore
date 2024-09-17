6d17398bceb512b249f93201548827de07cc8187
Borislav <borislav.m.angelov@gmail.com>
2024-09-17 17:32:25 +0300

    refactor: Add tables related to authentication

--- src/database/dbScript.sql
+++ src/database/dbScript.sql
@@ -7,12 +7,31 @@ DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS currencies;
 
+DROP TABLE IF EXISTS iso_country_codes;
+DROP TABLE IF EXISTS email_verifications;
+DROP TABLE IF EXISTS captchas;
+DROP TABLE IF EXISTS failed_attempts;
+DROP TABLE IF EXISTS attempt_types;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS session_types;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS genders;
+DROP TABLE IF EXISTS currencies;
+
+
 CREATE TABLE products (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     price NUMERIC(12, 2) NOT NULL,
     short_description TEXT NOT NULL,
     long_description TEXT NOT NULL
+);
+
+CREATE TABLE currencies (
+    id BIGSERIAL PRIMARY KEY,
+    currency_code TEXT NOT NULL,
+    exchange_rate_to_base NUMERIC(18, 6) NOT NULL, 
+    symbol TEXT NOT NULL,
 );
 
 CREATE TABLE currencies (
@@ -69,3 +88,72 @@ CREATE TABLE iso_country_codes (
     country_code TEXT UNIQUE NOT NULL,      
     phone_code TEXT NOT NULL                   
 );
+
+CREATE TABLE genders (
+    id BIGSERIAL PRIMARY KEY,
+    type TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    user_hash UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
+    password_hash TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phone TEXT NOT NULL,
+    iso_country_code_id BIGINT NOT NULL REFERENCES iso_country_codes(id), 
+    gender_id BIGINT NULL REFERENCES genders(id),
+    address TEXT NULL,
+    is_email_verified BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE session_types (
+    id BIGSERIAL PRIMARY KEY,
+    type TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE sessions (
+    id BIGSERIAL PRIMARY KEY,
+    session_hash UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
+    user_id BIGINT NULL REFERENCES users(id),
+    ip_address TEXT NULL,
+    session_type_id BIGINT NOT NULL REFERENCES session_types(id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    expires_at TIMESTAMPTZ NOT NULL DEFAULT NOW() + INTERVAL '10 minutes',
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE captchas (
+    id BIGSERIAL PRIMARY KEY,
+    session_id BIGINT NOT NULL REFERENCES sessions(id),
+    equation TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    expires_at TIMESTAMPTZ NOT NULL DEFAULT NOW() + INTERVAL '10 minutes',
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE attempt_types (
+    id BIGSERIAL PRIMARY KEY,
+    type TEXT UNIQUE NOT NULL -- 'login', 'captcha', etc.
+);
+
+CREATE TABLE failed_attempts (
+    id BIGSERIAL PRIMARY KEY,
+    session_id BIGINT NOT NULL REFERENCES sessions(id),
+    attempt_type_id BIGINT NOT NULL REFERENCES attempt_types(id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE email_verifications (
+    id BIGSERIAL PRIMARY KEY,
+    token_hash UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    expires_at TIMESTAMPTZ NOT NULL DEFAULT NOW() + INTERVAL '10 minutes',
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+INSERT INTO genders(type) VALUES ('Male'), ('Female');
+INSERT INTO session_types(type) VALUES ('Anonymous'), ('Authenticated'), ('Email Verification');
+INSERT INTO attempt_types(type) VALUES ('Login'), ('Captcha');
