db30e1e55448586bd85cea48fd4cc39ea8b14c5a
Borislav <borislav.m.angelov@gmail.com>
2024-09-20 18:52:35 +0300

    feat: Add backoffice server configuration and routing

---
+++ src/serverConfigurations/backofficeServerConfiguration.js
@@ -0,0 +1,119 @@
+const pool = require("../database/dbConfig");
+const entitySchemaCollection = require("../schemas/entitySchemaCollection");
+const { UserError } = require("../serverConfigurations/assert");
+
+const CrudService = require("../services/crudService");
+const CrudController = require("../controllers/crudController");
+const ProductService = require("../services/productService");
+const ProductController = require("../controllers/productController");
+const AuthService = require("../services/authService");
+const AuthController = require("../controllers/authController");
+const { MailService, transporter } = require("../services/mailService");
+const { DbConnectionWrapper } = require("../database/DbConnectionWrapper");
+
+const mailService = new MailService(transporter);
+const authService = new AuthService(mailService);
+const authController = new AuthController(authService);
+const service = new CrudService();
+const controller = new CrudController(service);
+const productService = new ProductService();
+const productController = new ProductController(productService);
+
+const routeTable = {
+  get: {
+    "/crud/:entity": controller.getAll,
+    "/crud/:entity/:id": controller.getById,
+    "/api/products" : productController.getFilteredPaginated,
+    "/auth/verify-mail": authController.verifyMail,
+    "/auth/status": authController.getStatus,
+    "/auth/logout": authController.logout,
+    "/auth/captcha": authController.getCaptcha,
+    "/api/products/:id/comments": productController.getComments,
+    "/api/products/:id/ratings": productController.getRatings,
+  },
+  post: {
+    "/crud/:entity": controller.create,
+    "/auth/register": authController.register,
+    "/auth/login": authController.login,
+  },
+  put: {
+    "/crud/:entity/:id": controller.update,
+    "/auth/profile": authController.updateProfile,
+  },
+  delete: {
+    "/crud/:entity/:id": controller.delete,
+  },
+};
+
+function registerRoutes(routing, app) {
+  Object.keys(routing).forEach((method) => {
+    Object.entries(routing[method]).forEach(([path, handler]) => {
+      app[method.toLowerCase()](path, requestMiddleware(handler));
+    });
+  });
+}
+
+function requestMiddleware(handler) {
+  return async (req, res, next) => {
+    try {
+      req.pool = pool;
+      req.dbConnection = new DbConnectionWrapper(await req.pool.connect());
+      req.entitySchemaCollection = entitySchemaCollection;
+
+      req.dbConnection.query("BEGIN");
+      await sessionMiddleware(req, res);
+      await handler(req, res, next);
+      req.dbConnection.query("COMMIT");
+
+    } catch (error) {
+      console.error(error);
+
+      if (req.dbConnection) {
+        req.dbConnection.query("ROLLBACK");
+      }
+      
+      if (error instanceof UserError) {
+        return res.status(400).json({ error: error.message });
+      } else {
+        return res.status(500).json({ error: "Internal server error" });
+      }
+    } finally {
+      if (req.dbConnection) {
+        req.dbConnection.release();
+      }
+    }
+  };
+}
+
+async function sessionMiddleware(req, res) {
+  let sessionId = req.cookies.session_id;
+
+  if (sessionId) {
+    const data = { dbConnection: req.dbConnection, sessionHash : sessionId };
+    const session = await authService.getSession(data);
+
+    if (session && new Date(session.expires_at) > new Date()) {
+      const refreshedSession = await authService.refreshSessionExpiry(data);
+      res.cookie('session_id', refreshedSession.session_hash, {
+        expires: refreshedSession.expires_at,
+        secure: true,
+        httpOnly: true
+      });
+      req.session = session;
+
+      return;
+    }
+  }
+
+  const data = { dbConnection: req.dbConnection, userId: null, ipAddress: req.ip, sessionType: 'Anonymous' };
+  const anonymousSession = await authService.createSession(data);
+  res.cookie('session_id', anonymousSession.session_hash, {
+    expires: anonymousSession.expires_at,
+    secure: true,
+    httpOnly: true
+  });
+
+  req.session = anonymousSession;
+}
+
+module.exports = { routeTable, sessionMiddleware, registerRoutes };
