29ae17ff5aa5349f945f07e826924a85f04b4cc5
Borislav <borislav.m.angelov@gmail.com>
2024-11-18 17:27:48 +0200

    feat: Update report page and sql query building

--- src/public/admin/report-order-by-user.js
+++ src/public/admin/report-order-by-user.js
@@ -1,44 +1,73 @@
-document
-  .getElementById("report-form")
-  .addEventListener("submit", async function (event) {
-    event.preventDefault();
-    const userName = document.getElementById("user_name").value;
-    const userId = document.getElementById("user_id").value;
-    const orderTotalMin = document.getElementById("order_total_min").value;
-    const orderTotalMax = document.getElementById("order_total_max").value;
+import { getUserStatus, attachLogoutHandler, hasPermission } from "./auth.js";
+import { createNavigation, createBackofficeNavigation } from "./navigation.js";
 
-    const response = await fetch("/api/reports/orders-by-user", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        user_name: userName,
-        user_id: userId,
-        order_total_min: orderTotalMin,
-        order_total_max: orderTotalMax,
-      }),
-    });
+const state = {
+  userStatus: null,
+}
 
-    const data = await response.json();
-    if (response.ok) {
-      const tbody = document.querySelector("#report-table tbody");
-      tbody.innerHTML = "";
-      data.forEach((row) => {
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
-                <td>${row.user_name}</td>
-                <td>${row.user_id}</td>
-                <td>${row.orders_last_day}</td>
-                <td>${row.total_last_day}</td>
-                <td>${row.orders_last_week}</td>
-                <td>${row.total_last_week}</td>
-                <td>${row.orders_last_month}</td>
-                <td>${row.total_last_month}</td>
-                <td>${row.orders_last_year}</td>
-                <td>${row.total_last_year}</td>
-            `;
-        tbody.appendChild(tr);
-      });
-    } else {
-      alert("Error: " + data.error);
-    }
+const elements = {
+  form: document.getElementById("report-form"),
+  table: document.getElementById("report-table"),
+  rowCount: document.getElementById("row-count"),
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const userStatus = await getUserStatus();
+  state.userStatus = userStatus;
+  createNavigation(userStatus);
+  await attachLogoutHandler();
+  createBackofficeNavigation(userStatus);
+
+  elements.form.addEventListener("submit", handleFilterFormSubmit);
+});
+
+async function handleFilterFormSubmit(event) {
+  event.preventDefault();
+  const userName = document.getElementById("user_name").value;
+  const userId = document.getElementById("user_id").value;
+  const orderTotalMin = document.getElementById("order_total_min").value;
+  const orderTotalMax = document.getElementById("order_total_max").value;
+
+  const response = await fetch("/api/reports/orders-by-user", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      user_name: userName,
+      user_id: userId,
+      order_total_min: orderTotalMin,
+      order_total_max: orderTotalMax,
+    }),
   });
+
+  const data = await response.json();
+  if (response.ok) {
+    const tbody = document.querySelector("#report-table tbody");
+    tbody.innerHTML = "";
+    data.forEach((row) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+              <td>${row.user_name}</td>
+              <td style="text-align: right">${row.user_id}</td>
+              <td style="text-align: right">${row.orders_last_day}</td>
+              <td style="text-align: right">${formatCurrency(row.total_last_day)}</td>
+              <td style="text-align: right">${row.orders_last_week}</td>
+              <td style="text-align: right">${formatCurrency(row.total_last_week)}</td>
+              <td style="text-align: right">${row.orders_last_month}</td>
+              <td style="text-align: right">${formatCurrency(row.total_last_month)}</td>
+              <td style="text-align: right">${row.orders_last_year}</td>
+              <td style="text-align: right">${formatCurrency(row.total_last_year)}</td>
+          `;
+      tbody.appendChild(tr);
+    });
+    elements.rowCount.textContent = `Showing ${data.length} rows`;
+  } else {
+    alert("Error: " + data.error);
+  }
+}
+
+function formatCurrency(number) {
+  if(!number) {
+    return '$0.00';
+  }
+  return `$${new Intl.NumberFormat('fr-FR', { minimumFractionDigits: 2, maximumFractionDigits: 2}).format(parseFloat(number)).replace(',', '.')}`;
+}
--- src/services/reportService.js
+++ src/services/reportService.js
@@ -1,89 +1,94 @@
-// src/services/reportService.js
 class ReportService {
-    constructor() {
-        
-    }
+  constructor() {}
 
-    async getOrdersByUserReport(data) {
-        const { body, dbConnection } = data;
-        const INPUT_DATA = {
-            "user_name_filter_value": body.user_name || '',
-            "user_id_filter_value": body.user_id || '',
-            "order_total_filter_value_min": body.order_total_min || '0',
-            "order_total_filter_value_max": body.order_total_max || '9999999',
-        };
+  async getOrdersByUserReport(data) {
+    const INPUT_DATA = {
+      user_name_filter_value: data.body.user_name || "",
+      user_id_filter_value: data.body.user_id || "",
+      order_total_minimum_filter_value: data.body.order_total_min || "0",
+      order_total_maximum_filter_value: data.body.order_total_max || "9999999",
+    };
 
-        const reportFilters = [
-            {
-                key: "user_name",
-                grouping_expression: "U.email",
-                filter_expression: "U.email ILIKE '%' || $FILTER_VALUE$ || '%'",
-                type: "text"
-            },
-            {
-                key: "user_id",
-                grouping_expression: "U.id",
-                filter_expression: "U.id = $FILTER_VALUE$",
-                type: "number"
-            },
-            {
-                key: "order_total",
-                grouping_expression: "NULL",
-                filter_expression: "O.total_price BETWEEN $FILTER_VALUE_MIN$ AND $FILTER_VALUE_MAX$",
-                type: "range_number"
-            }
-        ];
+    const reportFilters = [
+        {
+            key: "user_name",
+            grouping_expression: "U.email",
+            filter_expression: "STRPOS(LOWER(CAST( U.email AS text )), LOWER( '$FILTER_VALUE$' )) > 0",
+            type: "text",
+        },
+        {
+            key: "user_id",
+            grouping_expression: "U.id",
+            filter_expression: "U.id = $FILTER_VALUE$",
+            type: "select",
+        },
+        {
+            key: "order_total_minimum",
+            grouping_expression: "NULL",
+            filter_expression: "O.total_price >= $FILTER_VALUE$",
+            type: "number",
+        },
+        {
+            key: "order_total_maximum",
+            grouping_expression: "NULL",
+            filter_expression: "O.total_price <= $FILTER_VALUE$",
+            type: "number",
+        },
+    ];
 
-        let sql = `
-            SELECT
-                $user_name_grouping_expression$ AS "user_name",
-                $user_id_grouping_expression$ AS "user_id",
-                COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 day' THEN 1 END) AS "orders_last_day",
-                COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 day' THEN O.total_price END), 0) AS "total_last_day",
-                COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 week' THEN 1 END) AS "orders_last_week",
-                COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 week' THEN O.total_price END), 0) AS "total_last_week",
-                COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 month' THEN 1 END) AS "orders_last_month",
-                COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 month' THEN O.total_price END), 0) AS "total_last_month",
-                COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 year' THEN 1 END) AS "orders_last_year",
-                COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 year' THEN O.total_price END), 0) AS "total_last_year"
-            FROM orders O
-            JOIN users U ON U.id = O.user_id
-            WHERE TRUE
-                AND $user_name_filter_expression$
-                AND $user_id_filter_expression$
-                AND $order_total_filter_expression$
-            GROUP BY $user_name_grouping_expression$, $user_id_grouping_expression$
-            ORDER BY $user_name_grouping_expression$ ASC
-        `;
+    let sql = `
+        SELECT
+            $user_name_grouping_expression$ AS "user_name",
+            $user_id_grouping_expression$ AS "user_id",
+            COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 day' THEN 1 END) AS "orders_last_day",
+            COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 day' THEN O.total_price END), 0) AS "total_last_day",
+            COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 week' THEN 1 END) AS "orders_last_week",
+            COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 week' THEN O.total_price END), 0) AS "total_last_week",
+            COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 month' THEN 1 END) AS "orders_last_month",
+            COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 month' THEN O.total_price END), 0) AS "total_last_month",
+            COUNT(CASE WHEN O.created_at >= NOW() - INTERVAL '1 year' THEN 1 END) AS "orders_last_year",
+            COALESCE(SUM(CASE WHEN O.created_at >= NOW() - INTERVAL '1 year' THEN O.total_price END), 0) AS "total_last_year"
+        FROM orders O
+        JOIN users U ON U.id = O.user_id
+        WHERE TRUE
+            AND $user_name_filter_expression$
+            AND $user_id_filter_expression$
+            AND $order_total_minimum_filter_expression$
+            AND $order_total_maximum_filter_expression$
+        GROUP BY 1, 2
+        ORDER BY $user_name_grouping_expression$ ASC`;
 
-        for (let reportFilter of reportFilters) {
-            // Handle grouping expression
-            if (reportFilter.grouping_expression === 'NULL') {
-                sql = sql.replace(new RegExp(`\\$${reportFilter.key}_grouping_expression\\$`, 'g'), 'NULL');
-            } else {
-                sql = sql.replace(new RegExp(`\\$${reportFilter.key}_grouping_expression\\$`, 'g'), reportFilter.grouping_expression);
-            }
-
-            // Handle filter expression
-            if (reportFilter.type === 'range_number') {
-                let min = INPUT_DATA[`${reportFilter.key}_filter_value_min`] || '0';
-                let max = INPUT_DATA[`${reportFilter.key}_filter_value_max`] || '9999999';
-                let filterExprReplaced = reportFilter.filter_expression
-                    .replace('$FILTER_VALUE_MIN$', min)
-                    .replace('$FILTER_VALUE_MAX$', max);
-                sql = sql.replace(new RegExp(`\\$${reportFilter.key}_filter_expression\\$`, 'g'), filterExprReplaced);
-            } else if (INPUT_DATA[`${reportFilter.key}_filter_value`]) {
-                let filterValue = INPUT_DATA[`${reportFilter.key}_filter_value`];
-                let filterExprReplaced = reportFilter.filter_expression.replace('$FILTER_VALUE$', filterValue);
-                sql = sql.replace(new RegExp(`\\$${reportFilter.key}_filter_expression\\$`, 'g'), filterExprReplaced);
-            } else {
-                sql = sql.replace(new RegExp(`\\$${reportFilter.key}_filter_expression\\$`, 'g'), 'TRUE');
-            }
+    for (let reportFilter of reportFilters) {
+        if ( ! reportFilter.grouping_expression) {
+            sql = sql.replaceAll(`$${reportFilter.key}_grouping_expression$`, '');
+        } else {
+            sql = sql.replaceAll(`$${reportFilter.key}_grouping_expression$`, reportFilter.grouping_expression);
         }
 
-        const result = await dbConnection.query(sql);
-        return result.rows;
+        if (INPUT_DATA[`${reportFilter.key}_filter_value`]) {
+            let filterExpr = reportFilter.filter_expression;
+            let filterValue = INPUT_DATA[`${reportFilter.key}_filter_value`];
+            let filterExprReplaced;
+
+            if (reportFilter.type === 'timestamp'){
+                let [beginTimestampValue, endTimestampValue] = filterValue.split('|||');
+                filterExprReplaced = filterExpr
+                    .replace('$FILTER_VALUE_START$', beginTimestampValue)
+                    .replace('$FILTER_VALUE_END$', endTimestampValue);
+            } else {
+                filterExprReplaced = filterExpr.replace('$FILTER_VALUE$', filterValue);
+            }
+
+            sql = sql.replace(`$${reportFilter.key}_filter_expression$`, filterExprReplaced);
+        } else {
+            sql = sql.replace(`$${reportFilter.key}_filter_expression$`, 'TRUE');
+        }
     }
+
+    const result = await data.dbConnection.query(sql);
+    return result.rows;
+    // return { rows: result.rows, filterSchema: reportFilters };
+  }
 }
 
 module.exports = ReportService;
