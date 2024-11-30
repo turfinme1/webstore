161e9825680bb2f118f5bd02f38a2a79de2f1438
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-11-30 13:28:25 +0200

    feat: Add report page with dynamic report rendering with reportBuilder

---
+++ src/public/admin/report.js
@@ -0,0 +1,44 @@
+import { getUserStatus, attachLogoutHandler, hasPermission } from "./auth.js";
+import { createNavigation, createBackofficeNavigation } from "./navigation.js";
+import { ReportBuilder } from "./report-builder.js";
+
+const state = {
+    userStatus: null,
+}
+
+const elements = {
+    mainContainer: document.getElementById('main-container'),
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    state.userStatus = await getUserStatus();
+    createNavigation(state.userStatus);
+    await attachLogoutHandler();
+    createBackofficeNavigation(state.userStatus);
+
+    const URLParams = new URLSearchParams(window.location.search);
+    const reportName = URLParams.get('report');
+
+    // if (!hasPermission(state.userStatus, "read", reportName)) {
+    //     elements.mainContainer.innerHTML = "<h1>You don't have permission to view this report</h1>";
+    //     return;
+    // }
+
+    const reportConfigResponse = await fetch(`/api/reports/${reportName}`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ metadataRequest: true}),
+        }
+    );
+    if ( ! reportConfigResponse.ok) {
+        elements.mainContainer.innerHTML = "<h1>Failed to load report</h1>";
+        return;
+    }
+
+    const reportConfig = await reportConfigResponse.json();
+    const reportUI = new ReportBuilder(reportConfig);
+    reportUI.render('main-container');
+});
