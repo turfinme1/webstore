7aef232a44288c5ac1e2340522b32d899668d6d4
Borislav <borislav.m.angelov@gmail.com>
2024-12-18 18:13:00 +0200

    #FEATURE-86c1av1jb feature: Add voucher table and view

---
+++ src/database/migrations/20241218152206_AddVoucherTable.sql
@@ -0,0 +1,30 @@
+-- Migration: AddVoucherTable
+-- Created at: 2024-12-18T15:22:06.315Z
+
+CREATE TABLE vouchers (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    discount_amount NUMERIC(12, 2) NOT NULL,
+    code TEXT NOT NULL,
+    start_date TIMESTAMPTZ NOT NULL,
+    end_date TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE OR REPLACE VIEW vouchers_view AS
+SELECT
+    vouchers.*
+FROM vouchers
+WHERE vouchers.is_active = TRUE
+ORDER BY vouchers.id DESC;
+
+INSERT INTO interfaces (name) VALUES
+('vouchers');
+
+INSERT INTO permissions (name, interface_id) VALUES
+('view', 12),
+('create', 12),
+('read', 12),
+('update', 12),
+('delete', 12);
