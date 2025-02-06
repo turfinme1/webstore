3fe926c35bab66e1d185c5ffeaa56c4ab8944b6f
Borislav <borislav.m.angelov@gmail.com>
2025-02-06 14:45:09 +0200

    #FEATURE-86c1uz44p feature: Add notification table and schemas

---
+++ src/services/notificationService.js
@@ -0,0 +1,37 @@
+const { ASSERT, ASSERT_USER } = require("../serverConfigurations/assert");
+
+class NotificationService {
+    constructor(){
+    }
+
+    async getNotificationByUserId(data){
+        ASSERT_USER(data.session.user_id, "You must be logged in to perform this action", { code: "SERVICE.NOTIFICATION.00001.UNAUTHORIZED_GET_NOTIFICATION", long_description: "You must be logged in to perform this action" });
+        const notifications = await data.dbConnection.query(
+            `UPDATE emails SET status = 'sent' WHERE recipient_id = $1 AND type = 'Notification' AND status IN ('pending', 'sent')`,
+            [data.session.user_id]
+        );
+
+        const allUserNotifications = await data.dbConnection.query(
+            `SELECT * FROM emails WHERE recipient_id = $1 AND type = 'Notification'`,
+            [data.session.user_id]
+        );
+
+        return allUserNotifications.rows;
+    }
+
+    async markAsRead(data) {
+        ASSERT_USER(data.session.user_id, "You must be logged in", { 
+            code: "SERVICE.NOTIFICATION.00002.UNAUTHORIZED_MARK_READ", 
+            long_description: "User must be logged in to mark notifications as read" 
+        });
+
+        await data.dbConnection.query(
+            `UPDATE emails 
+             SET status = 'seen' 
+             WHERE id = $1 AND recipient_id = $2`,
+            [data.params.id, data.session.user_id]
+        );
+    }
+}
+
+module.exports = NotificationService;
