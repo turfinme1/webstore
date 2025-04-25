806c6b078749fca92e755607277c85ce66eff2d1
Borislav <borislav.m.angelov@gmail.com>
2025-04-25 15:40:15 +0300

    #BUG-get-status-fix bug: Fix tests

--- src/services/__tests__/authService.test.js
+++ src/services/__tests__/authService.test.js
@@ -328,7 +328,7 @@ describe("AuthService", () => {
 
       // Expected query
       const expectedQuery = `
-        SELECT u.first_name, u.last_name, u.email, u.iso_country_code_id, u.phone, u.gender_id, u.birth_date, u.country_id, u.address, u.has_first_login, st.type as session_type
+        SELECT u.first_name, u.last_name, u.email, u.has_first_login, st.type as session_type
         FROM sessions s
         JOIN session_types st ON s.session_type_id = st.id
         LEFT JOIN users u ON s.user_id = u.id
@@ -358,7 +358,7 @@ describe("AuthService", () => {
 
       // Expected query
       const expectedQuery = `
-        SELECT u.first_name, u.last_name, u.email, u.iso_country_code_id, u.phone, u.gender_id, u.birth_date, u.country_id, u.address, u.has_first_login, st.type as session_type
+        SELECT u.first_name, u.last_name, u.email, u.has_first_login, st.type as session_type
         FROM sessions s
         JOIN session_types st ON s.session_type_id = st.id
         LEFT JOIN users u ON s.user_id = u.id
@@ -385,7 +385,7 @@ describe("AuthService", () => {
 
       // Ensure the query was still called
       const expectedQuery = `
-        SELECT u.first_name, u.last_name, u.email, u.iso_country_code_id, u.phone, u.gender_id, u.birth_date, u.country_id, u.address, u.has_first_login, st.type as session_type
+        SELECT u.first_name, u.last_name, u.email, u.has_first_login, st.type as session_type
         FROM sessions s
         JOIN session_types st ON s.session_type_id = st.id
         LEFT JOIN users u ON s.user_id = u.id
