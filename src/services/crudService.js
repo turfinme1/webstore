3bd35fa13e880c739c6686d9ac40da99c86e5d18
Borislav <borislav.m.angelov@gmail.com>
2025-05-14 10:48:34 +0300

    #BUG-event-type bug: Add message_event_types table

--- src/services/crudService.js
+++ src/services/crudService.js
@@ -751,8 +751,8 @@ class CrudService {
         }
         
         await data.dbConnection.query(
-          `INSERT INTO message_queue (recipient_id, recipient_email, subject, text_content, notification_id, type) 
-          VALUES ($1, $2, $3, $4, $5, $6)`,
+          `INSERT INTO message_queue (recipient_id, recipient_email, subject, text_content, notification_id, type, event_type) 
+          VALUES ($1, $2, $3, $4, $5, $6, 'message')`,
           [user.id, user.email, template.subject, text_content, mainEntity.id, template.type]
         );
     }
